8e8872d837bfed8e2dc59c69584c8978a64bae60
Your Name <you@example.com>
2017-08-15 19:15:06 -0400

    One more try ,dasdadsada

--- blog/url.py
+++ blog/url.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='publish_post'),
     url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
-    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^drafts/$', views.post_list_draft, name='post_list_draft'),
 
 
         ]
