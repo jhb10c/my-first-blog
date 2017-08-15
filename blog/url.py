7f18f4b21e3731e87d189febcb49f90d5d0509ad
Your Name <you@example.com>
2017-08-15 16:48:50 -0400

    Added a delete function and publish function to posts

--- blog/url.py
+++ blog/url.py
@@ -6,9 +6,8 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='publish_post'),
+    url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
 
-
-]
-
-'''url(r'^$', views.post_list, name='post_list') assigns a view called post_list
-to  the URL ^$, which is the empty string.'''
+        ]
